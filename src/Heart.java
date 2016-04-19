fcdde59098ef2db87f15008611f32b398914ef04
fladdu <frasermann@sympatico.ca>
2016-04-18 23:35:50 -0400

    fixed file paths

---
+++ src/Heart.java
@@ -0,0 +1,14 @@
+
+public class Heart extends Organ{
+	private double bpm;
+
+	public Heart(double strength, double weight, double length){
+		//TODO: init strength length and weight and location and essential somewhere up the high-archie
+		//figure out how to strength --> speed
+	}
+
+	public void doFunction(double input){
+		//TODO: check if input is less than speed
+		//do stuff
+	}
+}
---
+++ src/Limb.java
@@ -0,0 +1,15 @@
+public abstract class Limb extends BodyPart{
+	private double strength;
+	private double length;
+	//private int numJoints;
+
+	public abstract void doFunction(double value);
+
+	public void getStrength(){
+		//TODO: add stuff;
+	}
+
+	public void getLength(){
+		//TODO: add stuff
+	}
+}
