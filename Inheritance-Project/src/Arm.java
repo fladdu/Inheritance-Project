95948a2ba20b449ae2eac2b2253dfd9a723bbe10
fladdu <frasermann@sympatico.ca>
2016-04-18 23:24:36 -0400

    Adding java files

---
+++ Inheritance-Project/src/Arm.java
@@ -0,0 +1,14 @@
+
+public class Arm extends Limb{
+
+	public Arm(double strength, double weight, double length){
+		//TODO: init strength length and weight and location and essential somewhere up the high-archie
+		//figure out how to strength --> lifting stuff
+		//also do gui stuff
+	}
+
+	public void doFunction(double input){
+		//TODO: check if input is less than lifting stuff
+		//do stuff
+	}
+}
---
+++ Inheritance-Project/src/Organ.java
@@ -0,0 +1,5 @@
+public abstract class Organ extends BodyPart{
+	private String bodySystem;
+
+	public abstract void doFunction();
+}
