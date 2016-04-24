0d7c2ec91117d6aa67d3a335f3fdc0e9e40f330a
fladdu <frasermann@sympatico.ca>
2016-04-23 22:38:32 -0400

    merge fix

--- src/main.java
+++ src/main.java
@@ -15,7 +15,6 @@ public class main {
 	public static void main(String[] args) {
 		GUI.setVisible(true);
 		JPanel infoPanel = new JPanel(null);
-		infoPanel.setVisible(false);
 		Container c = GUI.getContentPane();
 		JPanel p = new JPanel();
 			p.setLayout(new BoxLayout(p, BoxLayout.Y_AXIS));
